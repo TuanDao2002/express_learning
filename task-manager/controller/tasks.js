b180a4ec71f7e5c79b6124a38f07620fc43d944b
TuanDao2002 <s3877347@rmit.edu.vn>
2022-06-02 12:16:53 +0700

    add error-handler

--- task-manager/controller/tasks.js
+++ task-manager/controller/tasks.js
@@ -6,64 +6,48 @@ const getAllTasks = asyncWrapper(async (req, res) => {
     res.status(200).json({ tasks });
 });
 
-const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(201).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params; // get id from request param by object destructuring
+    const task = await Task.findOne({ _id: taskID }); // find the document matched with the required id
+
+    if (!task) {
+        return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
     }
-};
 
-const getTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params; // get id from request param by object destructuring
-        const task = await Task.findOne({ _id: taskID }); // find the document matched with the required id
+    res.status(200).json({ task });
+});
 
-        if (!task) {
-            return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
-        }
+const updateTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true, // always return the new updated object
+        runValidators: true, // always validate the attributes of the object
+        useFindAndModify: false, // not show warning message
+    });
 
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+    if (!task) {
+        return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
     }
-};
 
-const updateTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-            new: true, // always return the new updated object
-            runValidators: true, // always validate the attributes of the object
-            useFindAndModify: false, // not show warning message
-        });
+    res.status(200).json({ task });
+});
 
-        if (!task) {
-            return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
-        }
+const deleteTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID }); // find the require ID and delete it
 
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
+    if (!task) {
+        return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
     }
-};
 
-const deleteTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskID }); // find the require ID and delete it
-
-        if (!task) {
-            return res.status(404).json({ msg: `No task with ID: ${taskID}` }); // return immediately the error message, not the success status
-        }
-
-        res.status(200).json({ task: null, status: "success" }); // do not need to return the deleted object
-        // can be: res.status(200).send()
-    } catch (error) {
-        res.status(500).json({ msg: error });
-    }
-};
+    res.status(200).json({ task: null, status: "success" }); // do not need to return the deleted object
+    // can be: res.status(200).send()
+});
 
 module.exports = {
     getAllTasks,
