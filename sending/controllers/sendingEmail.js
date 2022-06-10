3e60275016b296a9a6cc844a2469e9d7514e928b
TuanDao2002 <s3877347@rmit.edu.vn>
2022-06-10 13:50:02 +0700

    update

--- sending/controllers/sendingEmail.js
+++ sending/controllers/sendingEmail.js
@@ -1,24 +1,28 @@
 const nodemailer = require("nodemailer");
 
+// NOTE: the email sent can be in trash bin
+
 const sendEmail = async (req, res) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com", // hostname
         port: 465,
-        secureConnection: false,
+        secure: true,
         auth: {
             user: process.env.USER,
             pass: process.env.PASS,
         },
-        tls: {
-            ciphers:'SSLv3'
-        }
     });
 
-    let info = await transporter.sendMail({
+    const MAX = 999999;
+    const MIN = 100000;
+
+    const verificationPass = Math.floor(Math.random() * (MAX - MIN) + MIN);
+
+    const info = await transporter.sendMail({
         from: process.env.USER,
         to: "s3877347@rmit.edu.vn",
-        subject: "Hello",
-        text: `${req.query.content}`,
+        subject: "Verification password",
+        text: `Your verification password is: ${verificationPass}`,
     });
 
     res.json(info);
